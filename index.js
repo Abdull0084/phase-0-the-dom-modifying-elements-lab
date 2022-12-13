92ac5f4cf7cb8874624b7a11e380dd8170f9948a
Abdull0084 <Abdimalik0084@gmail.com>
2022-12-13 17:30:54 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+let mainElement = document.querySelector('main#main');
+
+
+mainElement.remove()
+
+
+let newHeader = document.createElement('h1');
+
+
+ let parentElement = document.body;
+
+ 
+
+
+
+ newHeader.id ='victory';
+ newHeader.innerHTML = "YOUR-NAME is the champion";
+ newHeader;
